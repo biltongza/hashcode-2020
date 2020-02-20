3f209d72f1b2d42e437f0fddf80a7679d44e0273
Markus Imelmann <markus.imelmann@entelect.co.za>
2020-02-20 21:11:27 +0200

    poes1

--- solution.js
+++ solution.js
@@ -34,23 +34,67 @@ var contents = fs.readFileSync(currentlySolving.problemFile, 'utf8');
 
 const firstRow = R.head(R.split('\n', contents));
 
-const books = Number(R.split(' ', firstRow)[0]);
+const numberOfBooks = Number(R.split(' ', firstRow)[0]);
 const numberOfLibraries = Number(R.split(' ', firstRow)[1]);
 const daysOfScanning = Number(R.split(' ', firstRow)[2]);
 
-const scoresOfBooks = R.split('\n', contents)[1];
+//1 2 3 6 5 4
+const secondRow = (R.split('\n', contents))[1];
+const scoresOfBooks = R.map(score => Number(score))(R.split(' ', secondRow));
 
+const getOrderedBooks = (unorderedBooks) => {
+  const books = R.split(' ', unorderedBooks);
+
+  const lastBook = books[books.length - 1];
+
+  const cleanedLastBook = lastBook.substring(0, lastBook.length - 1);
+  books[books.length - 1] = cleanedLastBook;
+
+  const booksAsNumbers = R.map(book => Number(book), books);
+
+
+  const booksWithDetails = [];
+
+  for (let i = 0 ; i < booksAsNumbers.length ; i++) {
+    booksWithDetails.push({
+      value: scoresOfBooks[i],
+      index: i
+    });
+  }
+
+
+  const diff = (a, b) => { return b.value - a.value; };
+  const orderedBooks = R.sort(diff, booksWithDetails); //=> [2, 4, 5, 7]
+
+  console.log(orderedBooks)
+
+  return orderedBooks;
+};
 
 const libraries = [];
 
 for (let i = 2; i <= (numberOfLibraries + 2) ; i += 2){
   const library = {
     libraryDetails: R.split('\n', contents)[i],
-    libraryBooks: R.split('\n', contents)[i + 1]
+    libraryBooks: getOrderedBooks(R.split('\n', contents)[i + 1])
   }
 
   libraries.push(library);
 }
+
+
+const calculateWeighting = (library) => {
+  const signupLength = Number(R.split(' ', library.libraryDetails)[1]);
+  const booksCanShip = Number(R.split(' ', library.libraryDetails)[2]);
+  const weight = (daysOfScanning - signupLength) * booksCanShip;
+
+  return weight;
+}
+
+for(let i = 0 ; i < libraries.length ; i++) {
+  libraries[i].weighting = calculateWeighting(libraries[i]);
+}
+
 
 
 // var file = fs.createWriteStream(someOutputFileName);
